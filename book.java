4c708887b11460f985161f75c8552b8a0810b64c
damiansolkowicz <damiansolkowicz@outlook.com>
2022-10-21 18:28:59 +0200

    v3

--- book.java
+++ book.java
@@ -5,4 +5,19 @@ public class book {
     String publisher;
     String isbn;
     int pages;
+
+    book(String bookTitle, String bookAuthor, int releaseDate, String bookPublisher, String bookIsbn, int bookPages) {
+    title=bookTitle;
+    author = bookAuthor;
+    releaseDatesedate = releaseDate;
+    publisher = bookPublisher;
+    isbn = bookIsbn;
+    pages = bookPages;
+    }
+
+    public void printInfo() {
+        String info = title +", " + author+", "+ releaseDatesedate + ", " +publisher + ", " + isbn +", "+ pages;
+        System.out.println(info);
+    }
 }
+
--- library.java
+++ library.java
@@ -1,23 +1,14 @@
 public class library {
     public static void main(String[] args) {
-        final String appName = "Biblioteka 0,2";
-        book book1= new book();
-        book1.title=" W pustyni i w puszczy";
-        book1.author="Henryk Sienkiewicz";
-        book1.releaseDatesedate=10;
-        book1.publisher="Greg";
-        book1.isbn="7593840394";
-        book1.pages= 88;
-        System.out.println(appName);
-        System.out.println("Książki dostępne w bibliotece:");
+        final String appName = "Biblioteka 0,3";
+        book book1= new book("W pustyni i w puszczy", "Henryk Sienkiewicz", 2010, "Greg", "957593", 1890);
+        book book2= new book("Bajki dla dzieci", "Henryk Sienkiewicz", 2012, "Greg", "957593", 98);
+        book book3= new book("Bajki dla dorosłych", "Henryk Inny ", 2010, "Greg", "957593", 759);
 
         System.out.println(appName);
         System.out.println("Książki dostępne w bibliotece:");
-        System.out.println(book1.title);
-        System.out.println(book1.author);
-        System.out.println(book1.releaseDatesedate);
-        System.out.println(book1.pages);
-        System.out.println(book1.publisher);
-        System.out.println(book1.isbn);
+        book1.printInfo();
+        book2.printInfo();
+        book3.printInfo();
     }
 }
